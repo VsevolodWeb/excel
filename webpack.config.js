a3c2c9972a52ad70105636bbc4fe3293d0a59b04
Vsevolod Ivanov <alexmercerq@gmail.com>
2021-02-27 21:18:23 +0300

    Finish project config

--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,37 @@ const faviconName = 'favicon.ico'
 
 const getFilename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
+const jsLoaders = () => {
+	const loaders = [
+		{
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+			},
+		},
+	]
+
+	if (isDev) {
+		loaders.push('eslint-loader')
+	}
+
+	return loaders
+}
+
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	mode: 'development',
 	entry: ['@babel/polyfill', './index.js'],
 	output: {
 		filename: getFilename('js'),
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
 	},
 	resolve: {
 		extensions: ['.js'],
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
 			'@core': path.resolve(__dirname, 'src/core'),
-		}
+		},
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
@@ -31,35 +48,33 @@ module.exports = {
 			favicon: faviconName,
 			minify: {
 				removeComments: isProd,
-				collapseWhitespace: isProd
-			}
+				collapseWhitespace: isProd,
+			},
 		}),
 		new CopyPlugin({
-			patterns: [{from: path.resolve(__dirname, `src/${faviconName}`), to: ''}]
+			patterns: [{from: path.resolve(__dirname, `src/${faviconName}`), to: ''}],
 		}),
 		new MiniCssExtractPlugin({
-			filename: getFilename('css')
-		})
+			filename: getFilename('css'),
+		}),
 	],
 	devtool: isDev ? 'source-map' : false,
 	devServer: {
 		port: 3000,
-		hot: isDev
+		hot: isDev,
 	},
 	module: {
 		rules: [
 			{
 				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 			},
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env']
-				},
-			}
-		]
-	}
+				use: jsLoaders(),
+			},
+		],
+	},
+	target: process.env.NODE_ENV === 'development' ? 'web' : 'browserslist',
 }
